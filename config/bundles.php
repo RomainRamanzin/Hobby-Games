21b068b2ca8326597271cad7942383d2e7ab4887
Sinan <sinan.yaz@hotmail.com>
2023-01-26 20:24:42 +0100

    mise en place login et register

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,6 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
 ];
