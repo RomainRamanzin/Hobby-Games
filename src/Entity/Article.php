4b226293c7aedd9d383f448aa3491472df90c57b
Romain Ramanzin <romain.ramanzin@gmail.com>
2023-02-08 19:27:02 +0100

    ajout des entitées et relations

---
+++ src/Entity/Article.php
@@ -0,0 +1,182 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticleRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ArticleRepository::class)]
+class Article
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $cover = null;
+
+    #[ORM\Column]
+    private ?bool $is_valided = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $publication_date = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $last_lodified_date = null;
+
+    #[ORM\Column]
+    private ?int $number_of_views = null;
+
+    #[ORM\OneToMany(mappedBy: 'article', targetEntity: Section::class)]
+    private Collection $sections;
+
+    #[ORM\ManyToOne(inversedBy: 'articles')]
+    private ?Game $game = null;
+
+    #[ORM\ManyToOne(inversedBy: 'articles_writed')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $writed_by = null;
+
+    #[ORM\ManyToOne(inversedBy: 'articles_validated')]
+    private ?User $validated_by = null;
+
+    public function __construct()
+    {
+        $this->sections = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCover(): ?string
+    {
+        return $this->cover;
+    }
+
+    public function setCover(string $cover): self
+    {
+        $this->cover = $cover;
+
+        return $this;
+    }
+
+    public function isIsValided(): ?bool
+    {
+        return $this->is_valided;
+    }
+
+    public function setIsValided(bool $is_valided): self
+    {
+        $this->is_valided = $is_valided;
+
+        return $this;
+    }
+
+    public function getPublicationDate(): ?\DateTimeInterface
+    {
+        return $this->publication_date;
+    }
+
+    public function setPublicationDate(\DateTimeInterface $publication_date): self
+    {
+        $this->publication_date = $publication_date;
+
+        return $this;
+    }
+
+    public function getLastLodifiedDate(): ?\DateTimeInterface
+    {
+        return $this->last_lodified_date;
+    }
+
+    public function setLastLodifiedDate(\DateTimeInterface $last_lodified_date): self
+    {
+        $this->last_lodified_date = $last_lodified_date;
+
+        return $this;
+    }
+
+    public function getNumberOfViews(): ?int
+    {
+        return $this->number_of_views;
+    }
+
+    public function setNumberOfViews(int $number_of_views): self
+    {
+        $this->number_of_views = $number_of_views;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Section>
+     */
+    public function getSections(): Collection
+    {
+        return $this->sections;
+    }
+
+    public function addSection(Section $section): self
+    {
+        if (!$this->sections->contains($section)) {
+            $this->sections->add($section);
+            $section->setArticle($this);
+        }
+
+        return $this;
+    }
+
+    public function removeSection(Section $section): self
+    {
+        if ($this->sections->removeElement($section)) {
+            // set the owning side to null (unless already changed)
+            if ($section->getArticle() === $this) {
+                $section->setArticle(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getGame(): ?Game
+    {
+        return $this->game;
+    }
+
+    public function setGame(?Game $game): self
+    {
+        $this->game = $game;
+
+        return $this;
+    }
+
+    public function getWritedBy(): ?User
+    {
+        return $this->writed_by;
+    }
+
+    public function setWritedBy(?User $writed_by): self
+    {
+        $this->writed_by = $writed_by;
+
+        return $this;
+    }
+
+    public function getValidatedBy(): ?User
+    {
+        return $this->validated_by;
+    }
+
+    public function setValidatedBy(?User $validated_by): self
+    {
+        $this->validated_by = $validated_by;
+
+        return $this;
+    }
+}
